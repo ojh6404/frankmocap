53ee08f67d9dde2981146cc1020be4c7808a124a
Jihoon Oh <ojh6404@gmail.com>
2024-02-03 01:29:34 +0900

    update

--- demo_handmocap.py
+++ demo_handmocap.py
@@ -1,21 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-import os, sys, shutil
-import os.path as osp
 import numpy as np
 import cv2
-import json
 import torch
-from torchvision.transforms import Normalize
 
-import mocap_utils.general_utils as gnu
 import mocap_utils.demo_utils as demo_utils
 
 from handmocap.hand_mocap_api import HandMocap
 from handmocap.hand_bbox_detector import HandBboxDetector
 
-import renderer.image_utils as imu
-from renderer.viewer2D import ImShow
 import time
 import argparse
 
@@ -32,32 +25,8 @@ class DemoOptions():
         default_checkpoint_hand = "./extra_data/hand_module/pretrained_weights/pose_shape_best.pth"
         parser.add_argument('--checkpoint_hand', required=False, default=default_checkpoint_hand, help='Path to pretrained checkpoint')
 
-        # input options
-        parser.add_argument('--input_path', type=str, default=None, help="""Path of video, image, or a folder where image files exists""")
-        parser.add_argument('--start_frame', type=int, default=0, help='given a sequence of frames, set the starting frame')
-        parser.add_argument('--end_frame', type=int, default=float('inf'), help='given a sequence of frames, set the last frame')
-        parser.add_argument('--pkl_dir', type=str, help='Path of storing pkl files that store the predicted results')
-        parser.add_argument('--openpose_dir', type=str, help='Directory of storing the prediction of openpose prediction')
-
-        # output options
-        parser.add_argument('--out_dir', type=str, default=None, help='Folder of output images.')
-        # parser.add_argument('--pklout', action='store_true', help='Export mocap output as pkl file')
-        parser.add_argument('--save_bbox_output', action='store_true', help='Save the bboxes in json files (bbox_xywh format)')
-        parser.add_argument('--save_pred_pkl', action='store_true', help='Save the predictions (bboxes, params, meshes in pkl format')
-        parser.add_argument("--save_mesh", action='store_true', help="Save the predicted vertices and faces")
-        parser.add_argument("--save_frame", action='store_true', help='Save the extracted frames from video input or webcam')
-
         # Other options
-        parser.add_argument('--single_person', action='store_true', help='Reconstruct only one person in the scene with the biggest bbox')
-        parser.add_argument('--no_display', action='store_true', help='Do not visualize output on the screen')
-        parser.add_argument('--no_video_out', action='store_true', help='Do not merge rendered frames to video (ffmpeg)')
         parser.add_argument('--smpl_dir', type=str, default='./extra_data/smpl/', help='Folder where smpl files are located.')
-        parser.add_argument('--skip', action='store_true', help='Skip there exist already processed outputs')
-        parser.add_argument('--video_url', type=str, default=None, help='URL of YouTube video, or image.')
-        parser.add_argument('--download', '-d', action='store_true', help='Download YouTube video first (in webvideo folder), and process it')
-
-        # Body mocap specific options
-        parser.add_argument('--use_smplx', action='store_true', help='Use SMPLX model for body mocap')
 
         # Hand mocap specific options
         parser.add_argument('--view_type', type=str, default='third_view', choices=['third_view', 'ego_centric'],
@@ -65,9 +34,6 @@ class DemoOptions():
         parser.add_argument('--crop_type', type=str, default='no_crop', choices=['hand_crop', 'no_crop'],
             help = """ 'hand_crop' means the hand are central cropped in input. (left hand should be flipped to right).
                         'no_crop' means hand detection is required to obtain hand bbox""")
-
-        # Whole motion capture (FrankMocap) specific options
-        parser.add_argument('--frankmocap_fast_mode', action='store_true', help="Use fast hand detection mode for whole body motion capture (frankmocap)")
 
         # renderer
         parser.add_argument("--renderer_type", type=str, default="opengl",
@@ -85,74 +51,26 @@ class DemoOptions():
 def run_hand_mocap(args, bbox_detector, hand_mocap, visualizer):
     #Set up input data (images or webcam)
     input_type, input_data = demo_utils.setup_input(args)
-    cur_frame = args.start_frame
+    cur_frame = 0
     video_frame = 0
 
     while True:
         # load data
-        load_bbox = False
+        start_time = time.time()
 
-        if input_type =='image_dir':
-            if cur_frame < len(input_data):
-                image_path = input_data[cur_frame]
-                img_original_bgr  = cv2.imread(image_path)
-            else:
-                img_original_bgr = None
-
-        elif input_type == 'bbox_dir':
-            if cur_frame < len(input_data):
-                print("Use pre-computed bounding boxes")
-                image_path = input_data[cur_frame]['image_path']
-                hand_bbox_list = input_data[cur_frame]['hand_bbox_list']
-                body_bbox_list = input_data[cur_frame]['body_bbox_list']
-                img_original_bgr  = cv2.imread(image_path)
-                load_bbox = True
-            else:
-                img_original_bgr = None
-
-        elif input_type == 'video':      
-            _, img_original_bgr = input_data.read()
-            if video_frame < cur_frame:
-                video_frame += 1
-                continue
-            # save the obtained video frames
-            image_path = osp.join(args.out_dir, "frames", f"{cur_frame:05d}.jpg")
-            if img_original_bgr is not None:
-                video_frame += 1
-                if args.save_frame:
-                    gnu.make_subdir(image_path)
-                    cv2.imwrite(image_path, img_original_bgr)
-        
-        elif input_type == 'webcam':
-            _, img_original_bgr = input_data.read()
-
-            if video_frame < cur_frame:
-                video_frame += 1
-                continue
-            # save the obtained video frames
-            # image_path = osp.join(args.out_dir, "frames", f"scene_{cur_frame:05d}.jpg")
-            # if img_original_bgr is not None:
-            #     video_frame += 1
-            #     if args.save_frame:
-            #         gnu.make_subdir(image_path)
-            #         cv2.imwrite(image_path, img_original_bgr)
-        else:
-            assert False, "Unknown input_type"
+        _, img_original_bgr = input_data.read()
+        if video_frame < cur_frame:
+            video_frame += 1
+            continue
 
         cur_frame +=1
-        if img_original_bgr is None or cur_frame > args.end_frame:
+        if img_original_bgr is None:
             break   
-        print("--------------------------------------")
 
         # bbox detection
-        if load_bbox:
-            body_pose_list = None
-            raw_hand_bboxes = None
-        elif args.crop_type == 'hand_crop':
+        if args.crop_type == 'hand_crop':
             # hand already cropped, thererore, no need for detection
             img_h, img_w = img_original_bgr.shape[:2]
-            body_pose_list = None
-            raw_hand_bboxes = None
             hand_bbox_list = [ dict(right_hand = np.array([0, 0, img_w, img_h])) ]
         else:            
             # Input images has other body part or hand not cropped.
@@ -160,11 +78,9 @@ def run_hand_mocap(args, bbox_detector, hand_mocap, visualizer):
             assert args.crop_type == 'no_crop'
             detect_output = bbox_detector.detect_hand_bbox(img_original_bgr.copy())
             print("detect_output", detect_output)
-            body_pose_list, body_bbox_list, hand_bbox_list, raw_hand_bboxes = detect_output
+            _, body_bbox_list, hand_bbox_list, _ = detect_output
         
         # save the obtained body & hand bbox to json file
-        # if args.save_bbox_output:
-        #     demo_utils.save_info_to_json(args, image_path, body_bbox_list, hand_bbox_list)
 
         if len(hand_bbox_list) < 1:
             # print(f"No hand deteced: {image_path}")
@@ -187,26 +103,8 @@ def run_hand_mocap(args, bbox_detector, hand_mocap, visualizer):
             pred_mesh_list = pred_mesh_list,
             hand_bbox_list = hand_bbox_list)
 
-        # show result in the screen
-        if not args.no_display:
-            res_img = res_img.astype(np.uint8)
-            ImShow(res_img)
-
-        # save the image (we can make an option here)
-        # if args.out_dir is not None:
-        #     demo_utils.save_res_img(args.out_dir, image_path, res_img)
-
-        # save predictions to pkl
-        # if args.save_pred_pkl:
-        #     demo_type = 'hand'
-        #     demo_utils.save_pred_to_pkl(
-        #         args, demo_type, image_path, body_bbox_list, hand_bbox_list, pred_output_list)
-
-        # print(f"Processed : {image_path}")
-        
-    #save images as a video
-    # if not args.no_video_out and input_type in ['video', 'webcam']:
-    #     demo_utils.gen_video_out(args.out_dir, args.seq_name)
+        time_elapsed = time.time() - start_time
+        print(f"FPS: {1/time_elapsed:.2f}")
 
     # When everything done, release the capture
     if input_type =='webcam' and input_data is not None:
@@ -217,6 +115,7 @@ def run_hand_mocap(args, bbox_detector, hand_mocap, visualizer):
 def main():
     args = DemoOptions().parse()
     args.use_smplx = True
+    args.input_path = "webcam"
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     assert torch.cuda.is_available(), "Current version only supports GPU"
